63abc3bf4279197659b462dba360f9b1459c649f
Christina Browne <112651196+ChrisBrowne01@users.noreply.github.com>
2026-03-19 02:08:55 +0000

    Fix access token response type in Spotify authorization URL

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -19,7 +19,7 @@ const Spotify = {
       window.history.pushState('Access Token', null, '/');
       return accessToken;
     } else {
-      const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`;
+      const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=${accessToken}&scope=playlist-modify-public&redirect_uri=${redirectUri}`;
       window.location = accessUrl;
     }
   },
